a2e4fd15dd2671f44197462d46f13c78c48bb7ac
Markus Petritz <petritzdesigns@gmail.com>
2016-05-05 19:06:57 +0200

    Query helper Get IP

---
+++ OrderSystem/Helper/QueryHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderSystem.Database;
+using OrderSystem.Enums;
+
+namespace OrderSystem.Helper
+{
+    public class QueryHelper
+    {
+        public static SelectQueryBuilder GetIpQuery()
+        {
+            SelectQueryBuilder query = new SelectQueryBuilder("information_schema.processlist", false);
+            query.SelectColumn("host").Where("ID", "connection_id()");
+            return query;
+        }
+    }
+}
---
+++ OrderSystem/OrderSystemTests1/Helper/QueryHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderSystem.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Helper.Tests
+{
+    [TestClass()]
+    public class QueryHelperTests
+    {
+        [TestMethod()]
+        public void GetIpQueryTest()
+        {
+            Assert.AreEqual("SELECT `host` FROM information_schema.processlist WHERE ID = connection_id() ",
+                QueryHelper.GetIpQuery().Statement);
+        }
+    }
+}
