a2e9d34be7faa3fb697560718eb9932649b98e90
Markus Petritz <petritzdesigns@gmail.com>
2016-05-05 13:42:42 +0200

    Created query builder tests

---
+++ OrderSystem/OrderSystemTests1/Database/QueryBuilderTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderSystem.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Database.Tests
+{
+    [TestClass()]
+    public class QueryBuilderTests
+    {
+        [TestMethod()]
+        public void QueryBuilderTest()
+        {
+            SelectQueryBuilder test1 = new SelectQueryBuilder("user");
+            test1.SelectDistinct();
+            test1.SelectColumn("test");
+
+            Assert.AreEqual("SELECT DISTINCT `test` FROM `user` ", test1.Statement);
+        }
+    }
+}
